8db071a1ebfe6d8694339559047495f045dfceef
lassi.patanen <lassi.patanen@gofore.com>
2022-10-20 08:19:50 +0300

    Display registration form after logging in

--- src/components/AuthenticatedContainer/AuthenticatedContainer.tsx
+++ src/components/AuthenticatedContainer/AuthenticatedContainer.tsx
@@ -2,14 +2,16 @@ import { Container } from '@chakra-ui/react';
 
 // components
 import NavBar from '../NavBar/NavBar';
-import PdfForm from '../PdfForm/PdfForm';
+import RegistrationDataForm from '../RegistrationDataForm/RegistrationDataForm';
+import { ProfileData } from '../../@types';
 
 export default function AuthenticatedContainer() {
+  let data: ProfileData | undefined;
   return (
     <>
       <NavBar />
       <Container maxW="container.xl" mt={6}>
-        <PdfForm />
+        <RegistrationDataForm profileApiData={data} />
       </Container>
     </>
   );
