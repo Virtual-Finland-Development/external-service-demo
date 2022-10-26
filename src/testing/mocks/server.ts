8347b732bcb39f5b016ddb54c78ff7d5034c81a7
Lauri Saarela <lauri.saarela@gofore.com>
2022-10-26 15:09:37 +0300

    removed some unused stuff

--- src/testing/mocks/server.ts
+++ src/testing/mocks/server.ts
@@ -3,12 +3,3 @@ import { handlers } from './handlers';
 
 // This configures a request mocking server with the given request handlers.
 export const server = setupServer(...handlers);
-server.listen({
-  onUnhandledRequest(req) {
-    console.error(
-      'Found an unhandled %s request to %s',
-      req.method,
-      req.url.href
-    );
-  },
-});
