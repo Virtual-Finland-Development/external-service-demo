5713fefb1cc5c5aea450c6b3a1427f7d6673f89b
Lauri Saarela <lauri.saarela@gofore.com>
2022-10-27 11:21:16 +0300

    more tests

--- src/components/Registration/Registration.test.tsx
+++ src/components/Registration/Registration.test.tsx
@@ -1,8 +1,8 @@
 import {
   customRender1,
-  waitForElementToBeRemoved,
   waitFor,
   screen,
+  act,
 } from '../../testing/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Registration from './Registration';
@@ -33,5 +33,39 @@ describe('<Registration />', () => {
     });
 
     expect(lastNameInput).toHaveValue('Duck');
+  });
+
+  test('User opens form preview, sends the form, form has been sent.', async () => {
+    customRender1(<Registration />);
+
+    // use fake timers (setTimout calls)
+    jest.useFakeTimers();
+
+    const previewButton = await screen.findByRole('button', {
+      name: /preview/i,
+    });
+    expect(previewButton).toBeInTheDocument();
+
+    userEvent.click(previewButton);
+
+    const sendButton = await screen.findByRole('button', {
+      name: /send/i,
+    });
+    expect(sendButton).toBeInTheDocument();
+
+    userEvent.click(sendButton);
+
+    // to tell the unit test that timers will update component's state
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    const sentHeader = await screen.findByRole('heading', {
+      name: /registration sent!/i,
+    });
+    expect(sentHeader).toBeInTheDocument();
+
+    // restore original JS timer behaviour
+    jest.useRealTimers();
   });
 });
