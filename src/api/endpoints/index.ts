59315eddd81b2fcab27c820feca5a6581b5738b7
lassi.patanen <lassi.patanen@gofore.com>
2022-11-07 20:17:32 +0200

    print stuff for testing purposes

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -4,3 +4,8 @@ export const AUTH_GW_BASE_URL =
 
 export const USER_API_BASE_URL =
   process.env.REACT_APP_USER_API_BASE_URL || 'http://localhost:5001';
+
+// TODO: Removed these after testing stuff
+console.log(process.env.REACT_APP_ENVIRONMENT);
+console.log(process.env.REACT_APP_AUTH_GW_BASE_URL);
+console.log(process.env.REACT_APP_USER_API_BASE_URL);
