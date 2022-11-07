a739ad53a7a915de6fd1d5166656cbce55f3b9b4
lassi.patanen <lassi.patanen@gofore.com>
2022-11-07 21:23:28 +0200

    Disable testing stuff in workflow

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -4,10 +4,3 @@ export const AUTH_GW_BASE_URL =
 
 export const USER_API_BASE_URL =
   process.env.REACT_APP_USER_API_BASE_URL || 'http://localhost:5001';
-
-// TODO: Removed these after testing stuff
-const DUMMY_API_URL = process.env.REACT_APP_DUMMY_API_URL;
-
-console.log(DUMMY_API_URL);
-console.log(AUTH_GW_BASE_URL);
-console.log(USER_API_BASE_URL);
