773b3a3a71236f0e37a158aa2a4466cb2ca940de
lassi.patanen <lassi.patanen@gofore.com>
2022-11-07 21:03:23 +0200

    testing .env file

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -6,7 +6,8 @@ export const USER_API_BASE_URL =
   process.env.REACT_APP_USER_API_BASE_URL || 'http://localhost:5001';
 
 // TODO: Removed these after testing stuff
-const BUILD_ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT;
-console.log(BUILD_ENVIRONMENT);
+const DUMMY_API_URL = process.env.REACT_APP_DUMMY_API_URL;
+
+console.log(DUMMY_API_URL);
 console.log(AUTH_GW_BASE_URL);
 console.log(USER_API_BASE_URL);
