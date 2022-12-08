dcd285b92167afdfd1b1ee974937ab3e821351b0
Lauri Saarela <lauri.saarela@gofore.com>
2022-12-08 15:36:36 +0200

    use custom github action to fetch pulumi outputs for env variavles + renamed USER_API -> USERS_API

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -1,10 +1,14 @@
+// utils
+import { removeTrailingSlash } from '../../utils';
+
 export const AUTH_GW_BASE_URL =
   process.env.REACT_APP_AUTH_GW_BASE_URL ||
   'https://q88uo5prmh.execute-api.eu-north-1.amazonaws.com';
 
-export const USER_API_BASE_URL =
-  process.env.REACT_APP_USER_API_BASE_URL || 'http://localhost:5001';
+export const USERS_API_BASE_URL = process.env.REACT_APP_USERS_API_BASE_URL
+  ? removeTrailingSlash(process.env.REACT_APP_USERS_API_BASE_URL)
+  : 'http://localhost:5001';
 
-export const TESTBED_API_BASE_URL =
-  process.env.REACT_APP_TESTBED_API_BASE_URL ||
-  'https://v2xw5b7nhqkduxcw6v56pas5yy0livjb.lambda-url.eu-north-1.on.aws';
+export const TESTBED_API_BASE_URL = process.env.REACT_APP_TESTBED_API_BASE_URL
+  ? removeTrailingSlash(process.env.REACT_APP_TESTBED_API_BASE_URL)
+  : 'https://v2xw5b7nhqkduxcw6v56pas5yy0livjb.lambda-url.eu-north-1.on.aws';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,8 @@ export const JSONSessionStorage = {
     sessionStorage.setItem(key, JSON.stringify(value));
   },
 };
+
+// Helper function to remove trailing slashes from string
+export function removeTrailingSlash(str: string) {
+  return str.endsWith('/') ? str.slice(0, -1) : str;
+}
