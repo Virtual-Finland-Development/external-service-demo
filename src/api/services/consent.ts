3396e429d4bb9e609c347fbaf845d22bdfd0e88f
Lassi Piironen <lsipii@kapsi.fi>
2023-01-06 18:51:56 +0200

    feat: add consent api tools (copy from a2f-project)

---
+++ src/api/services/consent.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import {
+  appContextUrlEncoded,
+  SESSION_STORAGE_AUTH_TOKENS,
+} from '../../constants';
+import { JSONSessionStorage } from '../../utils/JSONStorage';
+import { AUTH_GW_BASE_URL } from '../endpoints';
+
+export type ConsentSituation = {
+  consentStatus: string;
+  consentToken?: string;
+  redirectUrl?: string;
+};
+
+export async function checkConsent(
+  dataSource: string
+): Promise<ConsentSituation> {
+  const idToken = JSONSessionStorage.get(SESSION_STORAGE_AUTH_TOKENS).idToken;
+
+  const response = await axios.post(
+    `${AUTH_GW_BASE_URL}/consents/testbed/consent-check`,
+    JSON.stringify({
+      appContext: appContextUrlEncoded,
+      dataSource: dataSource,
+    }),
+    {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    }
+  );
+
+  return response.data;
+}
+
+export function directToConsentService(consentSituation: ConsentSituation) {
+  if (!consentSituation.redirectUrl) {
+    throw new Error('Invalid consent situation');
+  }
+  window.location.assign(consentSituation.redirectUrl);
+}
